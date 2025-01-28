2cdec24b1c7e216b7e8db46f6d9b8dcc9b71f917
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-01-28 12:01:03 +0200

    Initial commit

---
+++ src/main/java/com/example/recipebox/RecipeBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recipebox/RecipeBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
