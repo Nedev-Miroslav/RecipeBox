fe43b098c22686be36d9fd22ca4ce00c750c679e
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-01-29 11:30:11 +0200

    configurating thymeleaf and bootstrap

---
+++ src/main/java/com/example/recipebox/web/MainController.java
@@ -0,0 +1,20 @@
+package com.example.recipebox.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+@CrossOrigin("*")
+@Controller
+public class MainController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+
+
+
+
+}
