7288b23819fb918920a719296710e13c2c736e93
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-01-30 20:24:30 +0200

    refactor recipe entity

--- src/main/java/com/example/recipebox/model/entity/Recipe.java
+++ src/main/java/com/example/recipebox/model/entity/Recipe.java
@@ -3,6 +3,7 @@ package com.example.recipebox.model.entity;
 import com.example.recipebox.model.enums.CategoryType;
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -31,8 +32,63 @@ public class Recipe extends BaseEntity{
     @ManyToMany(mappedBy = "favorites")
     private List<User> likedByUsers;
 
+    public Recipe() {
+        this.likedByUsers = new ArrayList<>();
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getIngredients() {
+        return ingredients;
+    }
 
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public CategoryType getCategoryType() {
+        return categoryType;
+    }
+
+    public void setCategoryType(CategoryType categoryType) {
+        this.categoryType = categoryType;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public List<User> getLikedByUsers() {
+        return likedByUsers;
+    }
+
+    public void setLikedByUsers(List<User> likedByUsers) {
+        this.likedByUsers = likedByUsers;
+    }
 }
