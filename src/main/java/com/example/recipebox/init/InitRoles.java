3098e65f8ec23e1f14802290930f4f2a9fc50961
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-02-03 14:15:09 +0200

    add InitAdmin file, InitRoles file and start making longin logic

---
+++ src/main/java/com/example/recipebox/init/InitRoles.java
@@ -0,0 +1,38 @@
+package com.example.recipebox.init;
+
+import com.example.recipebox.model.entity.Role;
+import com.example.recipebox.model.enums.RoleType;
+import com.example.recipebox.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(1)
+public class InitRoles implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    public InitRoles(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        long countRoles = this.roleRepository.count();
+
+        if (countRoles != 0) {
+            return;
+        }
+
+        Role admin = new Role();
+        admin.setRoleType(RoleType.ADMIN);
+        roleRepository.saveAndFlush(admin);
+        Role lawyer = new Role();
+        lawyer.setRoleType(RoleType.USER);
+        roleRepository.saveAndFlush(lawyer);
+
+
+    }
+}
---
+++ src/main/java/com/example/recipebox/service/impl/LoggedUserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.recipebox.service.impl;
+
+import com.example.recipebox.service.LoggedUserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoggedUserServiceImpl implements LoggedUserService {
+}
