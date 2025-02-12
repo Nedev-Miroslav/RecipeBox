242bb14a1992001919f4c0ede310a2bd521e4508
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-02-12 19:46:13 +0200

    Implements I18N

---
+++ src/main/java/com/example/recipebox/config/I18NWebConfig.java
@@ -0,0 +1,24 @@
+package com.example.recipebox.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+
+@Configuration
+public class I18NWebConfig implements WebMvcConfigurer {
+
+    private final LocaleChangeInterceptor localeChangeInterceptor;
+
+    public I18NWebConfig(LocaleChangeInterceptor localeChangeInterceptor) {
+        this.localeChangeInterceptor = localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor);
+    }
+
+
+}
